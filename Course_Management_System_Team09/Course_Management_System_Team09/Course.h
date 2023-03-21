44ab5ba3cdf1864f79eb501882bb5471d0ee2af1
Duy22CLC02 <127386181+Duy22CLC02@users.noreply.github.com>
2023-03-21 20:59:56 +0700

    Course Struct Create

---
+++ Course_Management_System_Team09/Course_Management_System_Team09/Course.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Student.h"
+#include "staff.h"
+using namespace std
+
+struct Course {
+    Student* students;
+    string courseName;
+    string courseID;
+    classes* classID;
+    string dayofWeek;
+    string sessionTime;
+    staff* teacher;
+    int numberofCredits;
+
+    void regis();
+	void ImportfromCSVfile();
+    void deletecourse();
+    void display();
+};
