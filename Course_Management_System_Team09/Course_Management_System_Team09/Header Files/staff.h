5dad249b5db370ac95f4377002ee9e9d5bf1609f
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-03-27 14:44:46 +0700

    Update staff.h

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
@@ -27,37 +27,6 @@ struct staff {
 	void coursemanage();
 };
 
-bool checkexist(staff* sHead, string id) {
-	while (sHead) {
-		if (sHead->id == id) return 1;
-		sHead = sHead->next;
-	}
-	return 0;
-}
 
-void regis(staff*& sHead, string id, string password) {
-	if (checkexist(sHead, id)) {
-		cout << "id da ton tai, vui long chon id khac" << endl;
-		return;
-	}
-	staff* temp = new staff;
-	temp->id = id;
-	temp->password = password;
-	temp->next = sHead;
-	sHead = temp;
-	cout << "Sign up successfully" << endl;
-}
-
-staff* login(staff* sHead, string id, string password) {
-	while (sHead) {
-		if (sHead->id == id && sHead->password == password) {
-			cout << "Login success" << endl;
-			return sHead;
-		}
-		sHead = sHead->next;
-	}
-	if (!sHead) cout << "ID doesn't exist" << endl;
-	return nullptr;
-}
 
 
