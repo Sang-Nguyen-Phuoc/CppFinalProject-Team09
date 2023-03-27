53ebdf5ea900a7f9cd0f7d574a36d6e26ea41aee
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-03-27 14:45:33 +0700

    Update staff.cpp

--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/staff.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/staff.cpp
@@ -5,65 +5,40 @@
 
 
 using namespace std;
-void staff::login() {
-	
-}
 
-void staff::regis() {
-
-}
-
-void staff::exit() {
-
-}
-
-void staff::logout() {
-
-}
-
-void staffmenu(staff a) {
-	system("cls");
-	int choice;
-	cout << "Welcome" <<a.id<< endl;
-	cout << "1.My account" << endl;
-	cout << "2.Staff's activities" << endl;
-	cout << "3.Log out" << endl;
-	cout << "Input your choice (1-3): ";
-	cin >> choice;
-	switch (choice) {
-		case 1:
-			system("cls");
-			cout << "Staff profile" << endl;
-			cout << "Your id: " << a.id << endl;
-			cout << "Your account's password: " << a.password << endl;
-		case 2:
-			system("cls");
-			int choice2;
-			cout << "1.Create school year" << endl;
-			cout << "2.Create new class" << endl;
-			cout << "3.Add student into class" << endl;
-			cout << "4.Import student from CSV file" << endl;
-			cout << "5.Create semester" << endl;
-			cout << "6.View a list of class" << endl;
-			cout << "7.View a list of all students in a class" << endl;
-			cout << "8.View a list of all courses" << endl;
-			cout << "9.View a list of students in a course" << endl;
-			cout << "10.End of a semester" << endl;
-			cout << "11.Back" << endl;
-			cout << "Input your choice (1-11): ";
-			cin >> choice2;
-			switch (choice2) {
-				case 1:
-					createschoolyear();
-				case 2:
-					createclass();
-				case 3:
-
-			}
-		case 3: 
-
+bool checkexist(staff* sHead, string id) {
+	while (sHead) {
+		if (sHead->id == id) return 1;
+		sHead = sHead->next;
 	}
+	return 0;
 }
+
+void regis(staff*& sHead, string id, string password) {
+	if (checkexist(sHead, id)) {
+		cout << "id da ton tai, vui long chon id khac" << endl;
+		return;
+	}
+	staff* temp = new staff;
+	temp->id = id;
+	temp->password = password;
+	temp->next = sHead;
+	sHead = temp;
+	cout << "Sign up successfully" << endl;
+}
+
+staff* login(staff* sHead, string id, string password) {
+	while (sHead) {
+		if (sHead->id == id && sHead->password == password) {
+			cout << "Login success" << endl;
+			return sHead;
+		}
+		sHead = sHead->next;
+	}
+	if (!sHead) cout << "ID doesn't exist" << endl;
+	return nullptr;
+}
+
 
 void createschoolyear() {
 	cout << "Enter school year: ";
