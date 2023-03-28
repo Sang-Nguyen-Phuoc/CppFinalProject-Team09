5bfd1c0df5be3f1b07abeb2611727dcb43f6bbbb
Sang-Nguyen-Phuoc <nguyenphuocsang04012004@gmail.com>
2023-03-28 22:54:25 +0700

    Update Display, add a student by keyboard and CSV file, adjust a bunch of attributes in student's struct

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/Student.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/Student.h
@@ -1,5 +1,9 @@
+#include <iostream> 
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct Student {
@@ -9,12 +13,20 @@ struct Student {
     std::string gender;
     std::string dateOfBirth;
     std::string socialID;
-    Student* next; 
+    Student* next;
 
-    void Register();
-    void Login();
+    void login(); 
+    static void AddStudentsFromCSVFile(string filename);
     void display();
+    void AddStudentFromKeyBoard();
     void viewCourses();
     void viewScoreBoard();
-
 };
+
+void Student::login(); 
+oid Student::AddStudentFromKeyBoard();
+void Student::AddStudentsFromCSVFile(string filename);
+void Student::display();
+void Student::viewCourses();
+void Student::viewScoreBoard();
+
--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Student.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Student.cpp
@@ -1,44 +1,114 @@
 #include "Student.h"
 
+// Declare a global pointer to the head of the linked list
+Student* head = nullptr;
 
-void Student::Login() {
+void Student::login() {
+    int id, password;
+    cout << "Enter your student ID: ";
+    cin >> id;
+    cout << "Enter your password: ";
+    cin >> password;
 
+    // For simplicity, we'll assume the password is always 1
+    if (id == studentID && password == 1) {
+        cout << "Login successful!" << endl;
+    } else {
+        cout << "Invalid student ID or password." << endl;
+    }
 }
 
-void Student::Register() {
 
+void Student::AddStudentFromKeyBoard() {
+    // Input student information from keyboard
+    Student* newStudent = new Student;
+    cout << "Enter student ID: ";
+    cin >> newStudent->studentID;
+    cin.ignore(); 
+    cout << "Enter first name: ";
+    cin >> newStudent->firstName;
+    cin.ignore(); 
+    cout << "Enter last name: ";
+    cin >> newStudent->lastName;
+    cin.ignore(); 
+    cout << "Enter gender: ";
+    cin >> newStudent->gender;
+    cin.ignore(); 
+    cout << "Enter date of birth: ";
+    cin >> newStudent->dateOfBirth;
+    cin.ignore(); 
+    cout << "Enter social ID: ";
+    cin >> newStudent->socialID;
+    cin.ignore();
+
+     // Check if the student ID is unique
+    Student* current = head;
+    while (current != nullptr) {
+        if (current->studentID == newStudent->studentID) {
+            cout << "Error: student ID already exists" << endl;
+            return;
+        }
+        current = current->next;
+    }
+    newStudent->next = head;
+    head = newStudent;
+}
+
+void Student::AddStudentsFromCSVFile(string filename) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Error: could not open file " << filename << endl;
+        return;
+    }
+    
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string field;
+        vector<string> fields;
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+        if (fields.size() != 6) {
+            cerr << "Error: invalid format in file " << filename << endl;
+            continue;
+        }
+        Student* newStudent = new Student;
+        newStudent->studentID = stoi(fields[0]);
+        newStudent->firstName = fields[1];
+        newStudent->lastName = fields[2];
+        newStudent->gender = fields[3];
+        newStudent->dateOfBirth = fields[4];
+        newStudent->socialID = fields[5];
+        newStudent->next = head;
+        head = newStudent;
+    }
+    file.close();
+}
+
+void Student::display() {
+    if (!head) {
+        cout << "The list is empty";
+        return;
+    }
+    Student* current = head;
+    while (current != nullptr) {
+        cout << "Student ID: " << current->studentID << endl;
+        cout << "First Name: " << current->firstName << endl;
+        cout << "Last Name: " << current->lastName << endl;
+        cout << "Gender: " << current->gender << endl;
+        cout << "Date of Birth: " << current->dateOfBirth << endl;
+        cout << "Social ID: " << current->socialID << endl << endl;
+        current = current->next;
+    }
 }
 
 void Student::viewCourses() {
-    if (numOfCourses == 0) {
-        cout << "You are not enrolled in any courses." << endl;
-        return;
-    }
-    cout << "Courses you are enrolled in:" << endl;
-    for (int i = 0; i < numOfCourses; i++) {
-        cout << i + 1 << ". " << course[i]->courseName << endl;
-    }
+    // View student's courses
+    // ...
 }
 
-void viewScoreboard() {
-    if (numOfCourses == 0) {
-        cout << "You are not enrolled in any courses." << endl;
-        return;
-    }
-    for (int i = 0; i < numOfCourses; i++) {
-        Course* c = course[i];
-        cout << "Course: " << c->courseName << endl;
-        cout << "Final Mark: " << c->getStudentFinalMark(id) << endl;
-        cout << "Midterm Mark: " << c->getStudentMidtermMark(id) << endl;
-        cout << "Other Mark: " << c->getStudentOtherMark(id) << endl;
-        cout << "Total Mark: " << c->getStudentTotalMark(id) << endl;
-        cout << endl;
-    }
-}
-void Student::display() {
-    std::cout << "ID: " << studentID << std::endl;
-    std::cout << "Name: " << firstName << " " << lastName << std::endl;
-    std::cout << "Gender: " << gender << std::endl;
-    std::cout << "Date of birth: " << dateOfBirth << std::endl;
-    std::cout << "Social ID: " << socialID << std::endl;
+void Student::viewScoreBoard() {
+    // View student's score board
+    // ...
 }
