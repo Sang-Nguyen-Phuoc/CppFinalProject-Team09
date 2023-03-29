68e85b61e6db313b55486ede67d899b369dadef4
Duy22CLC02 <127386181+Duy22CLC02@users.noreply.github.com>
2023-03-29 09:18:35 +0700

    Update Functions for struct Course

--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Course.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Course.cpp
@@ -1,7 +1,15 @@
 #include "Course.h"
 
-void Course::regis(){
-
+void Course::init(){
+    students = nullptr;
+    courseName = "";
+    courseID = "";
+    classID = nullptr;
+    dayofWeek = "";
+    sessionTime = "";
+    teacher = nullptr;
+    score = nullptr;
+    numberofCredits = 0;
 }
 	
 void Course::ImportfromCSVfile() {
@@ -12,6 +20,43 @@ void Course::display(){
     
 }
 
-void Course::updateCourse() {
+void Course::updateCourse(){
+    cout << "Enter new course name: ";
+    cin >> courseName;
 
+    cout << "Enter new course ID: ";
+    cin >> courseID;
+
+    cout << "Enter new class ID: ";
+    if (classID == nullptr) {
+        classID = new classes;
+    }
+    cin >> classID->classID;
+
+    cout << "Enter new day of week: ";
+    cin >> dayofWeek;
+
+    cout << "Enter new session time: ";
+    cin >> sessionTime;
+
+    cout << "Enter new teacher's name: ";
+    if (teacher == nullptr) {
+        teacher = new staff;
+    }
+    cin >> teacher->name;
+
+    cout << "Enter new number of credits: ";
+    cin >> numberofCredits;
+}
+
+void Course::deleteCourse(){
+    delete[] students;
+    students = nullptr;
+    delete classID;
+    classID = nullptr;
+    delete teacher;
+    teacher = nullptr;
+    delete[] score;
+    score = nullptr;
+    numberofCredits = 0;
 }
