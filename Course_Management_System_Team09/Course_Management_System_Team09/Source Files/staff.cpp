0eaed2a32fdf8d76004089ab764d436da5ae988c
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-05 12:50:20 +0700

    Update staff.cpp

--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/staff.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/staff.cpp
@@ -49,10 +49,121 @@ staff* login(staff* sHead, string id, string password) {
 	return nullptr;
 }
 
-void createschoolyear(string& schoolyear) {
+
+void createschoolyear(schoolyear& sy) {
+	if (sy.year.length()) {
+		cout << "schoolyear existed";
+		cin.ignore();
+		cin.get();
+		return;
+	}
 	cout << "Input schoolyear: ";
-	getline(cin, schoolyear);
+	cin.ignore();
+	getline(cin, sy.year);
+	cout << "School year begin from:";
+	cin.ignore();
+	getline(cin, sy.datestart);
+	cout << "To:";
+	cin.ignore();
+	getline(cin, sy.dateend);
+	ofstream ofs;
+	ofs.open("schoolyear.txt.");
+	ofs << sy.year << " " << sy.datestart << " " << sy.dateend;
+	ofs.close();
+	cout << "create success";
 	cin.get();
+}
+
+void deleteschoolyear(schoolyear& sy) {
+	if (!sy.year.length()) {
+		cout << "No existed schoolyear";
+		cin.get();
+		return;
+	}
+	fstream f;
+	f.open("schoolyear.txt", ios::out);
+	f << "";
+	f.close();
+	sy.year = "";
+	cout << "Delete success";
+	cin.get();
+}
+
+void createsemester(schoolyear sy, semester& s) {
+	if (!sy.year.length()) {
+		cout << "No schoolyear existed";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	if (s.s1 || s.s2 || s.s3) {
+		cout << "Semester in progress";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	int choice;
+	do {
+		cout << "Choose semester you want to create" << endl;
+		cout << "1.Semester 1" << endl;
+		cout << "2.Semester 2" << endl;
+		cout << "3.Semester 3" << endl;
+		cout << "4.Back" << endl;
+		cout << "Input your choice: ";
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			system("cls");
+			s.s1 = 1;
+			cout << "Create success";
+			addsemester(sy, s);
+			cin.ignore();
+			cin.get();
+			return;
+		case 2:
+			system("cls");
+			s.s2 = 1;
+			cout << "Create success";
+			addsemester(sy, s);
+			cin.ignore();
+			cin.get();
+			return;
+		case 3:
+			system("cls");
+			s.s3 = 1;
+			cout << "Create success";
+			addsemester(sy, s);
+			cin.ignore();
+			cin.get();
+			return;
+		}
+	} while (choice != 4);
+}
+
+void addsemester(schoolyear sy, semester s) {
+	ofstream ofs;
+	ofs.open("schoolyear.txt");
+	ofs << sy.year << " " << sy.datestart << " " << sy.dateend;
+	ofs << s.s1 << " " << s.s2 << " " << s.s3;
+	ofs.close();
+}
+
+void endsemester(schoolyear sy, semester& s) {
+	if (!s.s1 && !s.s2 && !s.s3) {
+		cout << "No semester in progress";
+		cin.ignore();
+		return;
+	}
+	s.s1 = 0;
+	s.s2 = 0;
+	s.s3 = 0;
+	ofstream ofs;
+	ofs.open("schoolyear.txt");
+	ofs << sy.year << " " << sy.datestart << " " << sy.dateend;
+	ofs << s.s1 << " " << s.s2 << " " << s.s3;
+	ofs.close();
+	cout << "The current semester has ended";
+	cin.ignore();
 }
 
 
@@ -70,4 +181,3 @@ void createclass(Class*& listclass) {
 		else break;
 	} while (true);
 }
-
