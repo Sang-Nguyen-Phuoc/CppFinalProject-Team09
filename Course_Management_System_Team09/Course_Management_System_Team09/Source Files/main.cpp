f575d509c0db23e2cfa5b9315de0926cf59fffc7
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-05 12:48:56 +0700

    Update main.cpp

--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/main.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/main.cpp
@@ -6,26 +6,6 @@
 using namespace std;
 
 int main() {
-	fstream staffaccount;
-	staffaccount.open("staffaccount.txt", fstream::in | fstream::out);
-	staff* sHead = nullptr;
-	if (!staffaccount.is_open())
-	{
-		cout << "Khong mo duoc file!";
-		return 0;
-	}
-	string id;
-	string password;
-	while (!staffaccount.eof()) {
-		staffaccount >> id;
-		staffaccount >> password;
-		staff* temp = new staff;
-		temp->id.assign(id);
-		temp->password.assign(password);
-		temp->next = sHead;
-		sHead = temp;
-	}
-	staffaccount.close();
-	menu(sHead);
+	menu();
 	return 0;
 }
