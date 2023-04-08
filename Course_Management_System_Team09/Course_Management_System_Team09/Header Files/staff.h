a5a3fc905bf1be12677d2a52fafeb8c41d7d211f
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-08 22:13:23 +0700

    Update staff.h

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
@@ -1,9 +1,11 @@
 #pragma once
 #include<string>
 #include"class.h"
+#include"semester.h"
+#include"schoolyear.h"
+#include"student.h"
 using namespace std;
 struct staff {
-	string name; 
 	string id;
 	string password;
 	staff* next = nullptr;
@@ -14,13 +16,15 @@ bool checkexist(staff* sHead, string id);
 void regis(staff*& sHead);
 
 staff* login(staff* s, string id, string password);
-void staffmenu(staff* a, staff*& sHead, schoolyear& sy, semester& s);
+void staffmenu(staff* a, staff*& sHead, schoolyear& sy, semester& s,Class*& c,student*& sSstart);
 
 void createschoolyear(schoolyear& sy);
 void deleteschoolyear(schoolyear& sy);
 void createsemester(schoolyear sy, semester& s);
 void addsemester(schoolyear sy, semester s);
 void endsemester(schoolyear sy, semester& s);
+void inputfile(semester s, student*& sStart);
+
 void createclass(Class*& listclass);
-
-
+void viewlistclass(Class* c);
+void viewlistofstudentinclass(Class* c, student* sStart);
