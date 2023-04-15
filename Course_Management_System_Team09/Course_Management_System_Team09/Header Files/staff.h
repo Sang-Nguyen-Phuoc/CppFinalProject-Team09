a4dea5ba2b042e9c63300ea509df93a48f036512
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-15 12:20:10 +0700

    Update staff.h

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/staff.h
@@ -1,30 +1,31 @@
 #pragma once
-#include<string>
-#include"class.h"
-#include"semester.h"
 #include"schoolyear.h"
+#include"class.h"
+#include"Course.h"
 #include"student.h"
+#include<string>
+
 using namespace std;
 struct staff {
 	string id;
 	string password;
 	staff* next = nullptr;
 };
-
 bool checkexist(staff* sHead, string id);
-
 void regis(staff*& sHead);
-
-staff* login(staff* s, string id, string password);
-void staffmenu(staff* a, staff*& sHead, schoolyear& sy, semester& s,Class*& c,student*& sSstart);
-
-void createschoolyear(schoolyear& sy);
-void deleteschoolyear(schoolyear& sy);
-void createsemester(schoolyear sy, semester& s);
-void addsemester(schoolyear sy, semester s);
-void endsemester(schoolyear sy, semester& s);
-void inputfile(semester s, student*& sStart);
-
 void createclass(Class*& listclass);
+void AddStudentFromKeyBoard(student*& head);
+void inputfile(schoolyear s, student*& sStart);
 void viewlistclass(Class* c);
 void viewlistofstudentinclass(Class* c, student* sStart);
+
+staff* login1(staff* sHead);
+
+void createschoolyear(schoolyear& s);
+void deleteschoolyear(schoolyear& s);
+void createsemester(schoolyear& s);
+
+void endsemester(schoolyear s);
+void createclass(Class*& listclass);
+
+void viewlistcourse(Course* course);
