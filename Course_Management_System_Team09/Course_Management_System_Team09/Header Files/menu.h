e468d72c31ba22639a421c531598610833d87b3a
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-15 12:19:44 +0700

    Update menu.h

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/menu.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/menu.h
@@ -5,5 +5,10 @@
 using namespace std;
 
 void menu();
+void input(staff*& sHead, schoolyear& s, Class*& c, student*& sStart, Course*& course);
+void output(staff* sHead, schoolyear s, Class* c, student* sStart, Course* cHead);
+
+void staffmenu(staff* a, staff*& sHead, schoolyear& s, Class*& c, student*& sStart, Course*& course);
+void studentmenu(student* s);
 
 
