37bde38c41aab2d7dbe977b253a0bef0ea9909c3
xiaohonsu <127416055+xiaohonsu@users.noreply.github.com>
2023-04-17 12:00:52 +0700

    Update Course.h

--- Course_Management_System_Team09/Course_Management_System_Team09/Header Files/Course.h
+++ Course_Management_System_Team09/Course_Management_System_Team09/Header Files/Course.h
@@ -1,8 +1,9 @@
+#pragma once
 #include <iostream>
 #include <string>
-#include "Student.h"                                        
-#include "staff.h"
-#include "classes.h"
+#include "student.h"
+#include"schoolyear.h"
+#include"class.h"
 using namespace std;
 
 struct Course {
@@ -13,10 +14,27 @@ struct Course {
 	string teacher;
 	int numberofCredits;
 	int numofstudent=0;
-	student* liststudent;
+	student* liststudent=nullptr;
+	schoolyear sc;
 	Course* next = nullptr;
 };
 
 void addStudentToCourse(Course* course);
 void deleteCourse(Course*& course);
-void addCourse(Course*& course);
+void addCourse(Course*& course,schoolyear s);
+void viewliststudentincourse(Course* course);
+void updatecourse(Course* course);
+void changecoursename(Course* course);
+void changecourseID(Course* course);
+void changedayofweek(Course* course);
+void changeteacher(Course* course);
+void changesession(Course* course);
+void addonestudent(Course* course);
+void deleteonestudent(Course* course);
+void viewcourse(Course* course, student* s,schoolyear sy);
+void getmark(Course* course,student* sHead);
+void viewscoreboard(Course* course,student*sHead);
+void updateresult(Course* course,student*sHead);
+void viewscoreofclass(Course* course, schoolyear sy, Class* c, student* sHead);
+void viewownscore(Course* course, student* s);
+void exportToCSVFile(Course* course, student* sHead);
