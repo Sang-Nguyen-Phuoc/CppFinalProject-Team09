af21d323e654f64efd97bf8f0165e94d678514dc
Duy22CLC02 <127386181+Duy22CLC02@users.noreply.github.com>
2023-04-22 19:29:29 +0700

    Custom Course UI

--- Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Student.cpp
+++ Course_Management_System_Team09/Course_Management_System_Team09/Source Files/Student.cpp
@@ -14,53 +14,59 @@ student* login2(student* sStart) {
 	cout << "Input password: ";
 	getline(cin, password);
 
+	cout << endl << setw(20) << "ID" << setw(20) << "First Name" << setw(20) << "Last Name" << endl;
+	cout << setfill('-') << setw(60) << "-" << endl;
+	cout << setfill(' ');
+
 	while (sStart) {
 		if (sStart->studentID == id && sStart->password == password) {
+			cout << setw(20) << sStart->studentID << setw(20) << sStart->firstName << setw(20) << sStart->lastName << endl;
 			cout << "Login success" << endl;
 			return sStart;
 		}
 		sStart = sStart->next;
 	}
-	if (!sStart) cout << "ID doesn't exist" << endl;
+
+	cout << "ID doesn't exist" << endl;
 	cout << "Press enter to continue";
+
 	return nullptr;
 }
 
-void viewcourse(Course* course,student* s,schoolyear sy) {
-	cin.ignore();
-	cout << "YOUR COURSE" << endl;
-	int i = 1;
-	cout << "#	Course ID	Course name		Number of credits	Schedule	Teacher" << endl;
-	while (course) {
-		student* temp = course->liststudent;
-		while (temp) {
-			if (temp->studentID == s->studentID) {
-				if (course->sc.s1 <= sy.s1 && course->sc.s2 <= sy.s2 && course->sc.s3 <= sy.s3) {
-					cout << i << "	" << course->courseID << "	" << course->courseName << "	" << course->numberofCredits << "	" << course->dayofWeek << " " << course->sessionTime << "	" << course->teacher << endl;
-					break;
-				}
-			}
-			temp = temp->next;
-		}
-		course = course->next;
-	}
-	cin.get();
-	
+
+void viewcourse(Course* course, student* s, schoolyear sy) {
+    cin.ignore();
+    cout << "YOUR COURSE" << endl;
+    int i = 1;
+    cout << setw(2) << "#" << setw(12) << "Course ID" << setw(20) << "Course name" << setw(20) << "Number of credits" << setw(20) << "Schedule" << setw(20) << "Teacher" << endl;
+    while (course) {
+        student* temp = course->liststudent;
+        while (temp) {
+            if (temp->studentID == s->studentID) {
+                if (course->sc.s1 <= sy.s1 && course->sc.s2 <= sy.s2 && course->sc.s3 <= sy.s3) {
+                    cout << setw(2) << i << setw(12) << course->courseID << setw(20) << course->courseName << setw(20) << course->numberofCredits << setw(20) << course->dayofWeek << " " << course->sessionTime << setw(20) << course->teacher << endl;
+                    break;
+                }
+            }
+            temp = temp->next;
+        }
+        course = course->next;
+    }
+    cin.get();
 }
 
 void viewownscore(Course* course, student* s) {
 	cin.ignore();
 	cout << "Your Scoreboard" << endl;
 	int i = 1;
-	cout << "#	Course name		Other mark	Midterm Mark	Final Mark	Total Mark" << endl;
+	cout << "#    Course name        Other mark    Midterm Mark    Final Mark    Total Mark" << endl;
 	coursedata* run = s->listcourse;
 	while (run) {
-		cout << i << "	" << run->courseName << "	" << run->otherMark << "	" << run->midtermMark << "	" << run->finalMark << "	" << run->totalMark << endl;
+		cout << i << "    " << setw(20) << left << run->courseName << "    " << setw(12) << left << run->otherMark << "    " << setw(14) << left << run->midtermMark << "    " << setw(11) << left << run->finalMark << "    " << setw(10) << left << run->totalMark << endl;
 		i++;
 		run = run->next;
 	}
 	cin.get();
 }
-
 
 
